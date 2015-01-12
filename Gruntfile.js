a1ecb0add2dc6ce5f51a805a695496675a541e32
vishnu aniyan <vishnu@linkstoweb.in>
2015-01-12 22:56:38 +0530

    Revert "Minified CSS for Build folder"
    This reverts commit a730eb579c9b21291886751094eefea524f02c3e.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,17 +21,6 @@ module.exports = function(grunt) {
           config: 'config.rb'
         } //options
       } //dev
-      dist: {
-                options: {
-                    sassDir: '_/components/scss',
-                    imagesDir: '/app/img',
-                    cssDir: 'build/css',
-                    javascriptsDir: 'build/lib',
-                    httpGeneratedImagesPath: 'build/img',
-                    noLineComments: true,
-                    outputStyle: 'compressed'
-                } //options
-            } //dist
     }, //compass
 
     watch: {
@@ -84,5 +73,4 @@ module.exports = function(grunt) {
 
   }) //initConfig
   grunt.registerTask('default', ['connect', 'watch']);
-  grunt.registerTask('dist', ['compass']);
 } //exports
